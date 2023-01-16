d742a8890486a70eef4f40675c743aa3bd6040fc
Vojta Stoklasa@cxmsx <vojtech.stoklasa@semantic-visions.com>
2023-01-16 18:21:57 +0100

    removed logins

--- export.sh
+++ export.sh
@@ -1,6 +1,7 @@
+# Add ftp password as argument
 filename="stoklasa-vojtech-sw-dev.pdf"
 curl \
 --request POST 'https://demo.gotenberg.dev/forms/chromium/convert/url' \
 --form 'url="http://ftp.vstkl.xyz/cv/index.html"' \
 -o $filename &&
-curl -T $filename ftp://ftp.vstkl.xyz/ftp.vstkl.xyz/cv/$filename --user 4181465:p3pVhnVt3k
+curl -T $filename ftp://ftp.vstkl.xyz/ftp.vstkl.xyz/cv/$filename --user 4181465:$1
