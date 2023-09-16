96b2d76e88bd87755d760383b278f34d9ee71d28
Vojta Stoklasa <dev@vstkl.xyz>
2023-09-16 08:16:50 +0200

    cleanup

--- export.sh
+++
@@ -1,7 +0,0 @@
-# Add ftp password as argument
-filename="stoklasa-vojtech-sw-dev.pdf"
-curl \
---request POST 'https://demo.gotenberg.dev/forms/chromium/convert/url' \
---form 'url="http://ftp.vstkl.xyz/cv/index.html"' \
--o $filename &&
-curl -T $filename ftp://ftp.vstkl.xyz/ftp.vstkl.xyz/cv/$filename --user 4181465:$1
